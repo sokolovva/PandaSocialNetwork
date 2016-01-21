797fff20baca61e19cf5b049bf14aea9223a5f20
sokolovva <petyasokolovva@gmail.com>
2016-01-21 20:49:40 +0200

    asas

---
+++ SocialNetwork/SocialNetwork.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork
+{
+    public class SocialNetwork
+    {
+       
+        
+        
+
+        public void AddPanda(object panda)
+        {
+          
+
+        }
+        public void HasPanda(object panda)
+        {
+
+        }
+
+
+
+    }
+}
