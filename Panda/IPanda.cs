bea30b5db366bbee33ee134b03212ed52bac2046
EdinPriqtel <vbarzin@gmail.com>
2016-01-23 13:36:13 +0200

    removed obj bin

--- Panda/IPanda.cs
+++ Panda/IPanda.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Panda
+﻿namespace Panda
 {
-    interface IPanda
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface IPanda
     {
+        string Name { get; set; }
+
+        string Email { get; set; }
+
+        Gender Gender { get; set; }
+
+        bool IsMale { get; set; }
+
+        bool IsFemale { get; set; }
+
+
     }
 }
